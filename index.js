264357f34f7c97d4eb5262ee9b667015f0a436ad
Bohdan Moyseyets <51541663+BohdanMoyseyets@users.noreply.github.com>
2023-10-28 16:25:23 +0300

    start commit

---
+++ index.js
@@ -0,0 +1,277 @@
+// console.log("hello world!12");
+
+// var zminna1;
+// let zminna2;
+// const zminna3 = 101;
+
+// let a = 12;
+
+// zminna2 = 13;
+// zminna1 = zminna2;
+// zminna2 = 200;
+// console.log(zminna1);
+// console.log(zminna2);
+// console.log(zminna3);
+// console.log(a);
+
+// string
+// number
+// undefined
+// boolean (true / false)
+// null
+// symbol
+// bigInt
+// object
+//  let obj = {
+//     name: "bohdan",
+//     age: 25
+// }
+
+// console.log(typeof(obj));
+// console.log(typeof("obj"));
+// console.log(typeof(1234));
+// console.log(typeof(undefined));
+// console.log(typeof(true));
+// console.log(typeof(false));
+// console.log(typeof(null));
+// console.log(typeof([]));
+
+// console.log(true == "aws");
+
+// // "if-else if-else
+// ? :
+// switch"
+
+// "window.addEventListener(""keydown"", function (event) {
+//     if (event.key === ""ArrowLeft"") {
+
+//     }
+// });"
+
+// let a = 4;
+// let b = 4;
+// let c = 7;
+// let d = 10;
+
+// // if (a > b && c < d){
+// //     console.log("a > b")
+
+// // } else if(a < b){
+
+// //     console.log("a < b ")
+
+// // }
+// // else{
+
+// //     console.log("a = b ")
+// // }
+
+// a == b ? console.log("yes") : console.log("no")
+
+// let day;
+// switch (3) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//     day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case  6:
+//     day = "Saturday";
+// }
+// if( document.getElementById("demo") != undefined ) {
+//     document.getElementById("demo").innerHTML = "Today is " + day;
+// }
+
+// console.log(123)
+
+// window.addEventListener("keydown", function (event) {
+//     if (event.key == "13") {
+//         console.log(123)
+//     }
+// });
+
+// document.body.onkeyup = function (e) {
+//     // if (e.keyCode == 40) {
+//     //     if (document.getElementById("demo") != undefined) {
+//     //         document.getElementById("demo").innerHTML = "arrow bottom clicked!";
+//     //     }
+//     // }
+//     document.getElementById("text").style.cssText = 'color:orange;background-color:yellow';
+//     switch (e.keyCode) {
+//         case 13:
+//             document.getElementById("demo").innerHTML = "enter clicked!";
+//             break;
+//         case 32:
+//             document.getElementById("demo").innerHTML = "spacebar clicked!";
+//             break;
+//         case 37:
+//             document.getElementById("demo").innerHTML = "arrow left clicked!";
+//             break;
+//         case 38:
+//             document.getElementById("demo").innerHTML = "arrow up clicked!";
+//             break;
+//         case 39:
+//             document.getElementById("demo").innerHTML = "arrow right clicked!";
+//             break;
+//         case 40:
+//             document.getElementById("demo").innerHTML = "arrow bottom clicked!";
+//     }
+// };
+
+
+
+
+
+
+
+// let array = [1, 2, 3, 4, 5];
+
+// let array2 = []
+
+
+// array.pop()
+// array.push(623)
+
+// let zmin = 123;
+
+
+// console.log(array);
+
+// for(let i=0; i<array.length; i++){
+//     array[i] = zmin;
+// }
+
+
+// let title = document.getElementsByTagName("h1");
+// let text_title = title[0].innerText;
+// console.log(text_title);
+
+
+// let array_students = [
+//     {
+//         name:"Bohdan",
+//         age: 25,
+//         location: "Lviv",
+//         cartd_type: "blue"
+//     },
+//     {
+//         name:"Tymofiy",
+//         age: 13,
+//         location: "Kyiv"
+//     },
+//     {
+//         name:"Danil",
+//         age: 14,
+//         location: "London"
+//     },
+//     {
+//         name:"Кирило",
+//         age: 15,
+//         location: "Kyiv"
+//     },
+//     {
+//         name:"Вероніка",
+//         age: 15,
+//         location: "Kyiv"
+//     },
+//     {
+//         name:"Andrii",
+//         age: 13,
+//         location: "Donetsk"
+//     },
+//     {
+//         name:"Вадим",
+//         age: 25,
+//         location: "Lviv"
+//     },
+//     {
+//         name:"Yuliia",
+//         age: 25,
+//         location: "Lviv"
+//     }
+// ];
+
+// console.log(bb);
+
+// let demo = document.getElementById("demo");
+// console.log(demo);
+// if(demo != undefined){
+//     for(let i=0; i<array_students.length; i++){
+//         demo.innerHTML +=` <div class="card">
+//         <img src="./img/avatar.jpeg" alt="avatar">
+//         <div class="name">${array_students[i].name}</div>
+//         <div class="age"><span>Age : </span> ${array_students[i].age}</div>
+//         <div class="location"> Location - ${array_students[i].location}</div>
+//     </div>`
+//     }
+
+
+//     for(let i=0; i<10; i++){
+//         // console.log(i);
+//     }
+
+//     let aa = 10;
+//     var bb = 11;
+
+//     let i=0;
+//     while(i<10){
+//         // console.log(i);
+//         i++;
+//     }
+// }
+// else{
+//     console.log("demo not found!!!!")
+// }
+
+// console.log(bb);
+
+
+
+// let a = 12;
+// const b = 1;
+
+// a > b ? console.log(" a - bilshe"): console.log("b - bilshe");
+
+// if(a>b){
+//     console.log(" a - bilshe")
+    
+// } else {
+//     console.log("b - bilshe")
+// }
+// console.time();
+
+
+// function test(a){
+//     console.log(a*10);
+// }
+
+
+// for(let i = 0; i < 10; i++){
+//     test(i)
+// }
+// console.timeEnd();
+
+
+// setTimeout(function (){
+//     console.log("time");
+// }, 5000)
+
+
+// console.log({} == {})
+// console.log([] == [])
+// console.log(1 == 1)
+
+console.log("deploy")
